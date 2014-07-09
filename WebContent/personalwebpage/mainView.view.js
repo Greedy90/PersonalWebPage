106bd4f5cd0f4f3ab6618bcb77e6418235d4b874
Greedy90 <marco.terrinoni90@gmail.com>
2014-07-09 21:36:36 +0200

    public key moved in the controller git repository in new tab

--- WebContent/personalwebpage/mainView.view.js
+++ WebContent/personalwebpage/mainView.view.js
@@ -44,13 +44,15 @@ function setContentGitHubPanel(oController) {
 	var gitHubLayout = new sap.ui.commons.layout.MatrixLayout({
 		columns: 2,
 		width: "90%",
-		widths: ["25%", "75%"]
+		widths: ["30%", "70%"]
 	});
 	
 	var gitLinkLabel = new sap.ui.commons.TextView({text: "You can find all my public works on this Git repository: "});
 	var gitLink = new sap.ui.commons.Link({
 		text: "Greedy90",
-		href: "https://github.com/Greedy90"
+		press: function(oEvent) {
+			window.open("https://github.com/Greedy90", "_blank");
+		}
 	});
 	
 	gitHubLayout.createRow(gitLinkLabel, gitLink);
@@ -92,37 +94,7 @@ function setContentContactsPanel(oController) {
 		press: function(oEvent) {
 			var pubKeyString = new sap.ui.commons.TextView({ // TODO - modify this control in order to retrieve the key directly from an external file
 				design: sap.ui.commons.TextViewDesign.Monospace,
-				text: "-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
-			  	   	  "Version: GnuPG v1\n" +
-			  	   	  "\n" +
-			  	   	  "mQENBFOJd2EBCADSCUN5tySll7y3wmBlTUy1fL6xf3T/r7Jeo2bMj0/I2xuV0mO8\n" +
-			  	   	  "qO3ZpNG2pTy7+yh3EqXRngfEHsB6W7Ym8gaMfIpFYsE/8JZOCTfLZp+w8qs0zkpk\n" +
-			  	   	  "sUgqPyc4MITrFG4xzBUX94IqrobWxk6lu4Jh6qIsVtjsdA4cptPOI5qigdbKGHJs\n" +
-			  	   	  "LGqM35r18DGjCw8DFNge6QqwZHWbIC/GXyjHWO2CMd56UOFHtSQqmrZK80X3bXx/\n" +
-			  	   	  "YoydKpJ6RNB4gKhm5HtwLWaqHDj5yrqkrFuWpPpS58C6Yo+sL8BZMoQ6L9y18125\n" +
-			  	   	  "yHQ/E3l1VyceV0fUojKfO2mraK0cA+hL3O7rABEBAAG0LU1hcmNvIFRlcnJpbm9u\n" +
-			  	   	  "aSA8bWFyY28udGVycmlub25pOTBAZ21haWwuY29tPokBPgQTAQIAKAUCU4l3YQIb\n" +
-			  	   	  "IwUJCWYBgAYLCQgHAwIGFQgCCQoLBBYCAwECHgECF4AACgkQmuWqXFWbjY2a9wf/\n" +
-			  	   	  "Z6p1CS/NGV+iZVrJf9BYZgIQVeOUeav2E21bLFQYbR6fLx6RlsQd8Mufo6JqKyHw\n" +
-			  	   	  "3aRWrrfH0Uy+BFXqrhuXQC5lhLadf+RLSyNf7GXTqE79eaZJDtkv1FV+6OYunBtG\n" +
-			  	   	  "wXDUqUjflQA+Bc9H150IeHNiaXI6Z8D3e9CHl3yfkithCoXCk+V2nHwkb2hB0k3P\n" +
-			  	   	  "JAaX5xkMj+ssKENgJPoq9QpGEyuUYVKWXTwQUvKiJnul3TSR/ekxgPsxGUxMSFHc\n" +
-			  	   	  "C+yo6ekw+q0Wc0LAPC5eEHm8ID9VWoNQOoet9oyrGK3U5iAuStFOZlP1JfAV8sut\n" +
-			  	   	  "9+faUx+NTtQZs+ikqNhWTLkBDQRTiXdhAQgA+SAyvmS+VJeY3MBj/lxQIAZd9a+u\n" +
-			  	   	  "mNV892WV+Ix5skMKzHV7J3fuR3R+BYKt81U/HU4nHtMCTIJkBni6OgG+5YnViCr+\n" +
-			  	   	  "rLsEFFqnVcNwgZPh/cB/ooJVBRrPYjfZLhr444VB/FAVd+vw699nraKYFc/lyRRr\n" +
-			  	   	  "+jY9mkH27ubDOi64GNzy2dSRhUz3tN+k6QNtuUgvfNkv7zAk41O+I8O7hqmglLXd\n" +
-			  	   	  "VvEeZsNP7WPOp55QZTbYFYkeiXsN+W94yaH6krYgTaDSdt4X3bOZI5pJSS68Oa3K\n" +
-			  	   	  "b1hrLnCcoop8TGhdR4Ilqm1nvRrEDYkTKSq8y7P8jn79tYHQ6dGfrmuxmQARAQAB\n" +
-			  	   	  "iQElBBgBAgAPBQJTiXdhAhsMBQkJZgGAAAoJEJrlqlxVm42NmBsIAJZJLkNtjakX\n" +
-			  	   	  "5Cm8PmV1jVMqSYejHsZ3jvY58iw9PnCjwHo/yXQV+DsKuNr/gV8bO88Kluvtw9ik\n" +
-			  	   	  "VG+rMBTzTE03Dr9naA3nWy0ac0uKRo0NwbMXx3gJgmdxc3mtzJQpxu5oRtXa5+VU\n" +
-			  	   	  "PoSVaswKVNXr1KkNfx0fCrxM+37siZ/Qx5EwPKEGLupxNE5ksMkhP61GewiWkbqL\n" +
-			  	   	  "b7HQbBvn2kJXbHmcBALaRyQ2IzMTKzPN9bWcSiAloilNHHR8vT9oa3tpyYKl+x5y\n" +
-			  	   	  "m/FXisAPe82f5mbOGyLIt3luBymPu3fs9dkeu6eNUI+7KYwMS9FmPGFWy/fVSRCG\n" +
-			  	   	  "DyWEPkGZpB4=\n" +
-			  	   	  "=iJgj\n" +
-			  	   	  "-----END PGP PUBLIC KEY BLOCK-----"
+				text: oController.getPubKey()
 			});
 			
 			var rsaKeyDialog = new sap.ui.commons.Dialog("rsaKeyDialogId", {
@@ -231,6 +203,7 @@ sap.ui.jsview("personalwebpage.mainView", {
 		
 		var mainShell = new sap.ui.ux3.Shell("mainShellId", {
 			appTitle: "Welcome to my home page",
+			appIcon: "images/faviconOrange.ico",
 			showLogoutButton: false,
 			showSearchTool: false,
 			showFeederTool: false,
